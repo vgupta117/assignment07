308795860b8aabedc0eb05585d09a81a9f2f37fe
Jaddy1 <jeffronaddy@gmail.com>
2021-02-12 19:43:33 -0500

    Files for the website

---
+++ static/scripts.js
@@ -0,0 +1,8 @@
+function showTable(){
+	var table = document.getElementById("popTable");
+	if(popTable.style.display === "none"){
+		popTable.style.display = "block";
+	}else{
+		popTable.style.display = "none";
+	}
+}
